513df458a7977ccb2813b209026805681ca3a3da
Rahul Kumar <rahulbenegal@gmail.com>
2011-11-16 22:07:02 +0530

    separated code into core extras experimental

--- widgets/rmulticontainer.rb
+++ widgets/rmulticontainer.rb
@@ -1,8 +1,8 @@
 =begin
-  * Name: Multiform.rb
-  * Description   View (cycle) multiple forms
+  * Name: MultiContainer 
+  * Description   View (cycle) multiple components in one container using a key or menu
   * Author: rkumar (arunachalesha)
-  * file created  2011-10-17 8:18 PM 
+  * file created 2010-03-15 10:40 
   --------
   * License:
     Same as Ruby's License (http://www.ruby-lang.org/LICENSE.txt)
@@ -14,19 +14,20 @@ include RubyCurses
 module RubyCurses
   extend self
 
-  # Trying to implement TabbedPanes using a multiform approach, but no more windows
-  # or pads. Try to keep it as simple as possible
-  class MultiForm < Widget
+  ##
+  # Extends TextView with ability to load more than one file or content
+  # and switch between files (buffers).
+  # NOTE: ideally, i should be able to dynamically add this functionality to either Textview
+  # or TextArea or even ListBox or Table someday. Should then be a Module rather than a class.
+  class MultiContainer < Widget
     dsl_accessor :title
 
 
     def initialize form = nil, config={}, &block
       @focusable = true
-      @window = form.window
       @row_offset = @col_offset = 1
-      @bmanager = BufferManager.new self
-      @forms = []
       super
+      @bmanager = BufferManager.new self
       init_vars
 
     end
@@ -45,23 +46,68 @@ module RubyCurses
       #bind_key(?p, :buffer_previous)
       @suppress_borders = false 
       @repaint_all = true 
-      @name ||= "multiform"
+      @name ||= "multicontainer"
     end
     ## returns current buffer
     # @return  [RBuffer] current buffer
     def current_component
       @bmanager.current
     end
+    ##
+    # Add a component with a title
+    # @param [Widget] component
+    # @param [String] title
+    def add component, title
+      component.row    = @row+@row_offset+0 # FFI changed 1 to 0 2011-09-12
+      component.col    = @col+@col_offset+0 # FFI changed 1 to 0 2011-09-12
+      component.width  = @width-2
+      component.height = @height-2
+      component.form   = @form
+      component.override_graphic(@graphic)
+      @current_buffer = @bmanager.add component, title
+      @current_component = @current_buffer.component
+      #set_current_component
+      #set_form_row ## FFI added 2011-09-12 to get cursor at start when adding
+      $log.debug "MULTICONT ADD got cb : #{@current_component} "
+    end
+    def set_current_component
+      @current_component = @current_buffer.component
+      @current_title = @current_component.title          # NOTE: unused, don't knw what for
+      set_form_row 
+      @current_component.repaint_all true
+    end
+    # required otherwise some components may not get correct cursor position on entry
+    # e.g. table
+    def on_enter
+      set_form_row
+    end
+    def set_form_row  #:nodoc:
+      if !@current_component.nil?
+        cc = @current_component
+    
+        @current_component.on_enter # 2011-10-19 why was this not there earlier
+
+        # 2011-10-21 I've tried removing next 2 lines but there are certain case
+        # that do need them. See testmulticontainer.rb
+        
+        @current_component.set_form_row 
+        @current_component.set_form_col 
+  
+      end
+    end
+    def set_form_col
+      # deliberately empty since Form will call this and Widgets one is unsuitable
+      # for us
+    end
     ## 
     # multi-container
     def handle_key ch  #:nodoc:
-      @current_component.repaint
-      @current_component.window.wrefresh
-      $log.debug " MULTI handle_key #{ch}, #{@current_component}"
+      $log.debug " MULTI handlekey #{ch}, #{@current_component}"
       ret = :UNHANDLED
       return :UNHANDLED unless @current_component
+
       ret = @current_component.handle_key(ch)
-      $log.debug " MULTI comp #{@current_component} returned #{ret} "
+      $log.debug " MULTI = current comp #{@current_component} returned #{ret} "
       if ret == :UNHANDLED
         # check for bindings, these cannot override above keys since placed at end
         begin
@@ -69,41 +115,38 @@ module RubyCurses
           $log.debug " MULTI = process_key returned #{ret} "
           if ch > 177 && ch < 187
             n = ch - 177
+      
             component_at(n)
+            ret = 0 # other unhandled goes back
             # go to component n
           end
         rescue => err
-#          $error_message = err # changed 2010 dts  
           $error_message.value = err.to_s
-          #@form.window.print_error_message PLEASE CREATE LABEL
           $log.error " Multicomponent process_key #{err} "
           $log.debug(err.backtrace.join("\n"))
           alert err.to_s
         end
         return :UNHANDLED if ret == :UNHANDLED
       end
-      @current_component.repaint
-      @current_component.window.wrefresh
-      FFI::NCurses.update_panels
-
       # check for any keys not handled and check our own ones
       return ret # 
     end
     def repaint
       print_border if (@suppress_borders == false && @repaint_all) # do this once only, unless everything changes
       return unless @current_component
-      $log.debug " MULTIFORM REPAINT "
+      $log.debug " MULTI REPAINT - calling current_comps repaint #{@current_component} "
       ret = @current_component.repaint
     end
     def print_border  #:nodoc:
-      $log.debug " #{@name} print_borders,  #{@graphic.name} "
+      #$log.debug " #{@name} print_borders,  #{@graphic.name} "
       color = $datacolor
       @graphic.print_border_only @row, @col, @height-1, @width, color #, Ncurses::A_REVERSE
       print_title
     end
     def print_title  #:nodoc:
-      $log.debug " print_title #{@row}, #{@col}, #{@width} #{@title} "
-      @graphic.printstring( @row, @col+(@width-@title.length)/2, @title, $datacolor, @title_attrib) unless @title.nil?
+      #$log.debug " print_title #{@row}, #{@col}, #{@width}  "
+      _title = @title || "" + @current_title
+      @graphic.printstring( @row, @col+(@width-_title.length)/2, _title, $datacolor, @title_attrib) unless _title.nil?
     end
     # this is just a test of the simple "most" menu
     # can use this for next, prev, first, last, new, delete, overwrite etc
@@ -127,26 +170,25 @@ module RubyCurses
     def goto_next_component
       perror "No other buffer" and return if @bmanager.size < 2
 
-      @current_component = @bmanager.next
+      @current_buffer = @bmanager.next
       set_current_component
-      # set_form_row and shit
     end
 
     def goto_prev_component
       perror "No other buffer" and return if @bmanager.size < 2
 
-      @current_component = @bmanager.previous
-      $log.debug " buffer_prev got #{@current_component} "
+      @current_buffer = @bmanager.previous
+      $log.debug " buffer_prev got #{@current_buffer} "
       set_current_component
     end
     def goto_first_component
-      @current_component = @bmanager.first
-      $log.debug " buffer_first got #{@current_component} "
+      @current_buffer = @bmanager.first
+      $log.debug " buffer_first got #{@current_buffer} "
       set_current_component
     end
     def goto_last_component
-      @current_component = @bmanager.last
-      $log.debug " buffer_last got #{@current_component} "
+      @current_buffer = @bmanager.last
+      $log.debug " buffer_last got #{@current_buffer} "
       set_current_component
     end
     def delete_component
@@ -166,56 +208,6 @@ module RubyCurses
       #$log.debug " buffer_last got #{@current_component} "
       set_current_component
     end
-    def add_form title, config={}, &blk
-      $log.debug "XXX: add_form window coords were hwtl #{@height} , #{@width} , #{@top} , #{@left} "
-      #w = VER::Window.new 16, 30, 2, 2 #@height-1, @width-1, @top, @left
-      w = VER::Window.new @height-2, @width-2, @row+1, @col+1
-      #w.box(0,0)
-      frm = Form.new w
-      @forms << frm
-      frm.parent_form = @form
-      frm.add_cols=@col+@col_offset
-      frm.add_rows=@row+@row_offset
-      @current_component = @bmanager.add frm, title
-      set_current_component
-      yield frm if block_given?
-      return @current_component # not a form but a Rcomponent containing component and title
-    end
-    def _add_to(index, component)
-      raise ArgumentError, "index out of bounds" unless @forms[index]
-      component.set_form(@forms[index])
-      $log.debug "XXX: comp r c #{component.row} #{component.col} "
-      component.row += @row
-      component.col += @col
-      $log.debug "XXX: after comp r c #{component.row} #{component.col} "
-    end
-    ##
-    # Add a component with a title
-    # @param [Widget] component
-    # @param [String] title
-    def add component, title
-      component.row = @row+@row_offset+0 # FFI changed 1 to 0 2011-09-12 
-      component.col = @col+@col_offset+0 # FFI changed 1 to 0 2011-09-12 
-      component.width = @width-2
-      component.height = @height-2
-      component.form = @form
-      component.override_graphic(@graphic)
-      @current_component = @bmanager.add component, title
-      set_current_component
-      set_form_row ## FFI added 2011-09-12 to get cursor at start when adding
-      $log.debug " ADD got cb : #{@current_component} "
-    end
-    def set_current_component
-      @title = @current_component.title
-      @current_component = @current_component.component
-      @current_component.repaint #2011-10-17 need to set all comps to repaint XXX
-      @current_component.window.wrefresh
-      @current_component.window.show
-      FFI::NCurses.update_panels
-      set_form_row
-      #@current_title = @current_component.title
-      #@current_component.repaint_all true 2011-10-17 need to set all comps to repaint XXX
-    end
     def perror errmess
       alert errmess
       #@form.window.print_error_message errmess
@@ -231,24 +223,6 @@ module RubyCurses
       }
       menu.display @form.window, $error_message_row, $error_message_col, $datacolor
     end
-    # required otherwise some components may not get correct cursor position on entry
-    # e.g. table
-    def on_enter
-      set_form_row # 2011-10-17 
-    end
-    # nothing happening, the cursor goes to 1,1 and does not move
-    # alhough the fields can be edited XXX
-    def set_form_row  #:nodoc:
-      if !@current_component.nil?
-        #$log.debug " #{@name} set_form_row calling sfr for #{@current_component.name} "
-        #fc = @current_component.widgets[0]
-        fc = @current_component.get_current_field
-        fc ||= @current_component.widgets[0]
-        fc.set_form_row  if fc
-        #fc.set_form_col 
-      end
-    end
-    # ADD HERE
   end # class multicontainer
   ##
   # Handles multiple buffers, navigation, maintenance etc
@@ -317,7 +291,7 @@ module RubyCurses
       return anew
     end
     def add component, title=nil
-      #$log.debug " ADD H: #{component.height} C: #{component.width} "
+      $log.debug " ADD H: #{component.height} C: #{component.width} "
       insert component, @buffers.size, title
     end
     def size
